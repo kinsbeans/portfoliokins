3de46399dd63f36ec137c99e1af014deaa264e8a
kinsbeans <112264902+kinsbeans@users.noreply.github.com>
2023-10-23 16:48:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ window.addEventListener('scroll', function() {
     let scrollPosition = window.pageYOffset;
     parallax.style.backgroundPositionY = -scrollPosition * 0.5 + 'px'; // Adjust the multiplier for the desired parallax effect speed
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sectionLinks = document.querySelectorAll('nav a');
+
+    sectionLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevent the default link behavior
+
+            // Remove the 'highlight' class from all sections
+            const sections = document.querySelectorAll('.card');
+            sections.forEach(function (section) {
+                section.classList.remove('highlight');
+            });
+
+            // Add the 'highlight' class to the target section
+            const targetSectionId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetSectionId);
+            targetSection.classList.add('highlight');
+
+            // Scroll to the target section smoothly
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
